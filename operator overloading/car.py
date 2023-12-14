84f5ecce8d9ba8f6da368d277fda164cd707fb5e
bhavanakbabu <146074730+bhavanakbabu@users.noreply.github.com>
2023-12-14 12:09:38 +0530

    Add files via upload

---
+++ operator overloading/car.py
@@ -0,0 +1,9 @@
+class Car:
+    def __init__(self, color, price,km):
+        self.color = color
+        self.price = price
+        self.km = km
+car1=car("yellow",1,00,000,5000)
+car2=car("black",
+        
+        
